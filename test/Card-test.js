7971a352182e14a19471e064c20c84888d3ef9ee
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-20 17:32:40 -0600

    Remove comment

--- test/Card-test.js
+++ test/Card-test.js
@@ -5,7 +5,6 @@ const Card = require('../src/Card');
 
 describe('Card', () => {
   let card;
-  
   beforeEach(() => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
   })
